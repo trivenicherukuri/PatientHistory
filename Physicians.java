d4a76a87f0c4be4c00e9e9965684ad09d559eb0b
trivenicherukuri <83311537+trivenicherukuri@users.noreply.github.com>
2021-04-28 02:03:38 +0530

    This file serves the physicians interface

---
+++ Physicians.java
@@ -0,0 +1,60 @@
+package com.psic;
+
+public class Physicians {
+
+	private int physicianID;
+	private String physicianName;
+	private String physicianAddress;
+	private int physicianPhNumber;
+	private String specialized;
+	private String workDate;
+
+	public int getPhysicianID() {
+		return physicianID;
+	}
+
+	public void setPhysicianID(int physicianID) {
+		this.physicianID = physicianID;
+	}
+
+	public String getPhysicianName() {
+		return physicianName;
+	}
+
+	public void setPhysicianName(String physicianName) {
+		this.physicianName = physicianName;
+	}
+
+	public String getPhysicianAddress() {
+		return physicianAddress;
+	}
+
+	public void setPhysicianAddress(String physicianAddress) {
+		this.physicianAddress = physicianAddress;
+	}
+
+	public int getPhysicianPhNumber() {
+		return physicianPhNumber;
+	}
+
+	public void setPhysicianPhNumber(int physicianPhNumber) {
+		this.physicianPhNumber = physicianPhNumber;
+	}
+
+	public String getSpecialized() {
+		return specialized;
+	}
+
+	public void setSpecialized(String specialized) {
+		this.specialized = specialized;
+	}
+
+	public String getWorkDate() {
+		return workDate;
+	}
+
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+
+}
